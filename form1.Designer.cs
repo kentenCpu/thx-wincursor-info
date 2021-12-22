4df2b15f90891327977172d8177c3e36681131fa
kentenCpu <96505472+kentenCpu@users.noreply.github.com>
2021-12-21 21:32:17 -0500

    first upload of light task
    first upload

---
+++ form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace thx_wincursor_info
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(310, 267);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(326, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 291);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinimumSize = new System.Drawing.Size(350, 300);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ form1.cs
@@ -0,0 +1,105 @@
+﻿/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Name                                                                                                        //
+// by KentenCpu | humanoid capable of self replicating GIF images;                                             //
+//                                                                                                             //
+// find this project on **** please do not copy material I create,,                                            //
+// thus see MS documentation; and dont do drugs and dont go to school, or do both, or do one or the other;     //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace thx_wincursor_info
+{
+    public partial class form1 : Form
+    {
+        public form1()
+        {
+            InitializeComponent();
+            start();
+        }
+        // a thread used to keep mouse information up to date, such as updating movement coordinates
+        Thread thr = new Thread(() => { });
+        // the mouse positions and display rectangles, such as location and width and height
+        Point mousecPos = new Point(0, 0);
+        Rectangle displayBounds = new Rectangle(0, 0, 0, 0);
+        // when captured the captured point will be kept in this variable for the text to display even if the mouse is moving.
+        Rectangle caputredRec = new Rectangle(0, 0, 0, 0);
+
+        // to have the mouse coordinates updated while off the main form, create an invisble form the size of the display (dont later)
+        Form formc = new Form();
+
+        // the startup method, create a thread to run starthread() function
+        void start()
+        {
+            formc.Bounds = formc.DesktopBounds;
+           
+            // create and start the update thread
+            thr = new Thread(starthread);
+            thr.IsBackground = true;
+            thr.Start();
+        }
+       
+        // the main thread function with the loop
+        void starthread()
+        {
+            // set some startup info for the captured mouse pos and display bounds, this will be for textBox1
+            caputredRec = new Rectangle(0, 0, formc.DesktopBounds.Width, formc.DesktopBounds.Width);
+            // set topmost and invisible
+            formc.Visible = false;
+            formc.TopMost = true;  // topmost or toplevel, or both should keep it topmost without updates
+            formc.TopLevel = true;
+            // add an event handler for the invisble formc
+            formc.MouseMove += new System.Windows.Forms.MouseEventHandler(formcMouseMove);
+            // debug, set some text though it should be replaced instatntly
+            textBox1.Text += "formc bounds are" + formc.Bounds.X.ToString() + formc.Bounds.Y.ToString();
+
+            // note while in threads, event handlers in c# seem to not work unless a workaround which there may be a better way
+            // though creating an action using methodinvoker from any control on the main form seems to do the trick
+            while (true)
+            {
+                Thread.Sleep(50);
+                displayBounds = Screen.GetBounds(formc);
+                textBox1.Invoke(new MethodInvoker(() =>
+                {
+                    textBox1.Text = String.Format(@"");
+                }));
+                textBox1.Invoke(new MethodInvoker(() =>
+                {
+                    textBox1.Text += String.Format(@"Mouse Coords: {0}, {1}", MousePosition.X, MousePosition.Y);
+                }));
+                textBox1.Invoke(new MethodInvoker(() => { textBox1.Text += Environment.NewLine; }));
+                textBox1.Invoke(new MethodInvoker(() =>
+                {
+                    textBox1.Text += String.Format(@"Display Dim: W,H,X,Y:{0}, {1}, {2}, {3}", displayBounds.Width, displayBounds.Height, MousePosition.X, MousePosition.Y);
+                }));
+                textBox1.Invoke(new MethodInvoker(() => { textBox1.Text += Environment.NewLine; }));
+                textBox1.Invoke(new MethodInvoker(() =>
+                {
+                    textBox1.Text += String.Format(@"Caputred Bounds: W,H,X,Y:{0}, {1}, {2}, {3}", caputredRec.Width, caputredRec.Height, caputredRec.X, caputredRec.Y);
+                }));
+                textBox1.Invoke(new MethodInvoker(() => { textBox1.Text += Environment.NewLine; }));
+            }
+        }
+
+        // event should do stuff on mouse movemnt, such as update the invisble form to a variable
+        void formcMouseMove(object sender, MouseEventArgs e)
+        {
+            formc.Bounds = formc.DesktopBounds;
+            displayBounds = Screen.GetBounds(this);
+        }
+
+
+
+
+        // anythin below is notes or unsued
+        private void textBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+
+            // mouseCurrent = 
+        }
+    }
+}
